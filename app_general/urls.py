877027386d780d4509cd5af3921df3f935af3b5a
mostmeow <nattawat_virunsunthornkul@hotmail.com>
2023-01-09 09:14:39 +0700

    update

--- app_general/urls.py
+++ app_general/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('listitem', views.listitem, name='listitem'),
 ]
--- app_general/views.py
+++ app_general/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 def home(request):
     return render(request, 'app_general/home.html')
+
+def listitem(request):
+    dummyarray = []
+    ranglist = range(100)
+
+    for i in ranglist:
+        dummyarray.append("a")
+
+    # p = Paginator(ProductModel.objects.all(), 8)
+    p = Paginator(dummyarray, 8)
+    page = request.GET.get('page')
+    pitems = p.get_page(page)
+    nums = 'a' * pitems.paginator.num_pages
+
+    context = {
+        # 'dummyarray':dummyarray,
+        'pitems':pitems,
+        'nums':nums,
+    }
+    return render(request, 'app_general/listitem.html', context)
